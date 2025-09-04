41b95f176b569fe029d0488b2a67bb2a99277640
ASUS <nethmidileksha6@gmail.com>
2025-09-04 19:45:27 +0530

    add public folder

--- src/components/PortfolioHomepage.tsx
+++ src/components/PortfolioHomepage.tsx
@@ -547,8 +547,8 @@ export default function PortfolioHomepage():any {
                     </div>
 
                     <a
-                        href="../../my-Cv/Dileksha_Kavindi_MyCV.pdf"
-                        download="Dileksha_Kavindi_MyCV.pdf"
+                        href="../../public/Dileksha_Kavindi_CV.pdf"
+                        download="Dileksha_Kavindi_CV.pdf"
                         className="inline-block"
                     >
                         <button className="group relative bg-gradient-to-r from-blue-500 to-purple-600 px-8 py-4 rounded-full font-medium flex items-center justify-center gap-3 transition-all duration-300 hover:scale-105 hover:shadow-lg hover:shadow-blue-500/25">
